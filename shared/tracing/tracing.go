209b8629084fa78c02fe6f6e2b9589dd5a24cfbb
facenach <ignacioijg@gmail.com>
2026-08-13 15:01:22 -0300

    first steps for adding Open telemetry

---
+++ shared/tracing/tracing.go
@@ -0,0 +1,56 @@
+package tracing
+
+import (
+	"context"
+	"fmt"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+)
+
+type Config struct {
+	ServiceName    string
+	Enviroment     string
+	JaegerEndPoint string
+}
+
+func InitTracer(cfg Config) (func(context.Context) error, error) {
+	// Exporter
+	traceExporter := nil
+
+	//Trace Provider
+	traceProvider, err := newTraceProvider(cfg, traceExporter)
+	if err != nil {
+		return nil, err
+	}
+	otel.SetTracerProvider(traceProvider)
+
+	//Propagator
+	propagator := newPropagator()
+	otel.SetTextMapPropagator(propagator)
+
+	return traceProvider.Shutdown, nil
+}
+
+func newTraceProvider(cfg Config, exporter sdktrace.SpanExporter) (*sdktrace.TracerProvider, error) {
+
+	res, err := resource.New(context.Background(),
+		resource.WithAttributes(semconv.ServiceNameKey.String(cfg.ServiceName),
+			semconv.DeploymentEnvironmentKey.String(cfg.Enviroment),
+		))
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to create resource: %v", err)
+	}
+
+	traceProvider := sdktrace.NewTracerProvider(sdktrace.WithBatcher(exporter), sdktrace.WithResource(res))
+
+	return traceProvider, nil
+}
+
+func newPropagator() propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{})
+}
